64402e9617167d1e2c48cd30c133a3c2bcc63464
Marina <marina-l-p@mail.ru>
2021-03-10 21:41:38 +1000

    Update bookingForm.php
    инпуты для даты

--- bookingForm.php
+++ bookingForm.php
@@ -4,8 +4,6 @@
     <?php
         include 'components/head.html';
     ?>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
 </head>
 <body>
     <?php
@@ -16,12 +14,12 @@
             <form class="form__order" action="search.php" method="post">
                 <h2 class="form__title">Бронирование</h2>
                 <div class="form__order-wrapper-text">
-                    <span class="form__order-label" type="date">Дата приезда</span>
-                    <input type="text" id="datepicker1" placeholder="Дата приезда">
+                    <span class="form__order-label">Дата приезда</span>
+                    <input type="date" placeholder="Дата приезда">
                 </div>
                 <div class="form__order-wrapper-text">
-                    <span class="form__order-label" type="date">Дата отъезда</span>
-                    <input type="text" id="datepicker2" placeholder="Дата отъезда">
+                    <span class="form__order-label">Дата отъезда</span>
+                    <input type="date" placeholder="Дата отъезда">
                 </div>
                 <p class="form__order-title">Выберите тип номера</p>
                 <div class="form__order-wrapper-text">
@@ -47,8 +45,5 @@
     <?php
         include 'components/footer.php';
     ?>
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="js/datepicker.js"></script>
 </body>
 </html>
